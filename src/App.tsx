7192cf600697af321131d4bc560dcbc9f9361a55
JoaoVG-Dev <devjoaog@outlook.com>
2026-05-20 16:40:57 -0300

    chore: setup React portfolio CMS structure

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <main className="app-shell">
+      <h1>João Vitor Guidoti</h1>
+      <p>Portfolio CMS em construção.</p>
+    </main>
+  );
+}
+
+export default App;
+
